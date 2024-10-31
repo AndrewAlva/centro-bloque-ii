022409f44801a58a981d0264f459dae6e1971329
Andrew Alvarado <andrew@activetheory.net>
2024-10-31 14:56:32 -0600

    Mic input example: added threshold interaction

--- 02-Mic-Input/main.js
+++ 02-Mic-Input/main.js
@@ -46,50 +46,21 @@ class App {
     }
 
     initMicInteraction() {
-        const canvas = this.canvas;
-        const ctx = this.ctx;
+        var _this = this;
 
         // Check if the browser supports audio input
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ audio: true })
                 .then(stream => {
-                    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-                    const analyser = audioContext.createAnalyser();
-                    analyser.fftSize = 256; // Size of the FFT. Higher values give more frequency bars.
+                    _this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+                    _this.analyser = _this.audioContext.createAnalyser();
+                    _this.analyser.fftSize = 256; // Size of the FFT. Higher values give more frequency bars.
 
-                    const source = audioContext.createMediaStreamSource(stream);
-                    source.connect(analyser);
+                    _this.source = _this.audioContext.createMediaStreamSource(stream);
+                    _this.source.connect(_this.analyser);
 
-                    const bufferLength = analyser.frequencyBinCount;
-                    const dataArray = new Uint8Array(bufferLength);
-
-                    function draw() {
-                        analyser.getByteFrequencyData(dataArray);
-
-                        // Clear canvas
-                        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-                        // Set bar properties
-                        const barWidth = (canvas.width / bufferLength) * 2.5;
-                        let barHeight;
-                        let x = 0;
-
-                        for (let i = 0; i < bufferLength; i++) {
-                            barHeight = dataArray[i];
-
-                            // Set color for each bar
-                            ctx.fillStyle = `rgb(${barHeight + 100}, 50, 200)`;
-                            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-
-                            // Move to the next bar position
-                            x += barWidth + 1;
-                        }
-
-                        // Call draw again to animate
-                        requestAnimationFrame(draw);
-                    }
-
-                    draw(); // Start the animation loop
+                    _this.bufferLength = _this.analyser.frequencyBinCount;
+                    _this.dataArray = new Uint8Array(_this.bufferLength);
                 })
                 .catch(error => {
                     console.error("Microphone access was denied or not available:", error);
@@ -97,6 +68,48 @@ class App {
         } else {
             alert("getUserMedia not supported on your browser!");
         }
+    }
+
+
+    draw() {
+        this.analyser.getByteFrequencyData(this.dataArray);
+
+        // Clear canvas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // Set bar properties
+        const barWidth = (this.canvas.width / this.bufferLength) * 1;
+        let barHeight;
+        let x = 0;
+
+
+        // Select "frequency" to be used as threshold or interaction
+        const frequencySelected = 20;
+        let frequencyIntensity = this.dataArray[frequencySelected];
+
+        // Light up the background when reaching certain height
+        let threshold = 70;
+        if (frequencyIntensity > threshold) {
+            this.ctx.fillStyle = `rgb(255, 250, 200)`;
+            this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        }
+
+        // Draw bars logic
+        for (let i = 0; i < this.bufferLength; i++) {
+            barHeight = this.dataArray[i];
+
+            // Set color for each bar based on frequency amplitude
+            this.ctx.fillStyle = `rgb(${barHeight + 100}, 50, 200)`;
+            this.ctx.fillRect(x, this.canvas.height - barHeight, barWidth, barHeight);
+
+            // Move to the next bar position
+            x += barWidth + 1;
+        }
+
+        // Draw the threshold mark on top of everything
+        let xPos = (barWidth + 1) * frequencySelected;
+        this.ctx.fillStyle = `rgb(0, 255, 255)`;
+        this.ctx.fillRect(xPos, this.canvas.height - threshold, barWidth, 2);
     }
 
 
@@ -110,6 +123,11 @@ class App {
 
 function frame() {
     // console.log("custom animation frame interval");
+
+    // Prevent drawing until Audio and Analyser were set up
+    if (myApp && myApp.analyser) {
+        myApp.draw();
+    }
 
     requestAnimationFrame(frame);
 }
